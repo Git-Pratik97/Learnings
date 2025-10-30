4b8419d2ceaa33dee8ae11e72c70714c8a8cd9ac
Git-Pratik97 <pratikkambleat@gmail.com>
2025-10-30 22:07:23 +0530

    Adding Collections topic -> Iterator

---
+++ src/collectionFramework/LearnIterator.java
@@ -0,0 +1,32 @@
+package collectionFramework;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class LearnIterator {
+    public static void main(String[] args) {
+        ArrayList<String> arr = new ArrayList<>(Arrays.asList("Apple", "Ball", "Cat"));
+
+        //Iterate using enhanced for loop
+        System.out.println("using Enhanced For loop");
+        for(String ele: arr){
+            System.out.println(ele);
+        }
+        System.out.println();
+
+        //Iterate using ForEach Iterator
+        System.out.println("Using For-Each Loop");
+        arr.forEach((ele) -> System.out.println(ele));
+        System.out.println();
+
+        System.out.println("Using Iterator...");
+        //Iterate using an Iterator
+        Iterator<String> iterator = arr.iterator();
+        while(iterator.hasNext()){
+            String element = iterator.next();
+            System.out.println(element);
+        }
+
+        }
+}
